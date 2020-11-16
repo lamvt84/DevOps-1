780b9170924b5c06176ce0f34e4941991484ef88
LouisVu84 <LouisVu84@users.noreply.github.com>
2020-11-16 14:51:16 +0700

    Commit

--- Dba/EDW/Doc/ScriptsToRun/EDWTargetSB_DB.sql
+++ Dba/EDW/Doc/ScriptsToRun/EDWTargetSB_DB.sql
@@ -28,6 +28,32 @@ ALTER DATABASE [$(DatabaseName)]
 USE [$(DatabaseName)]
 GO
 
-/* Table 
+/* Table */
+CREATE TABLE [dbo].[BrokerConversation] (
+    [ConversationHandle] UNIQUEIDENTIFIER NULL,
+    [ServiceName]        NVARCHAR (512)   NULL
+);
+GO
 
-*/
+CREATE TABLE [dbo].[BrokerMessages] (
+    [Id]                  INT              IDENTITY (1, 1) NOT NULL,
+    [ConversationHandler] UNIQUEIDENTIFIER NULL,
+    [MessageTypeName]     NVARCHAR (256)   NULL,
+    [MessageBody]         XML              NULL
+);
+GO
+
+CREATE TABLE [dbo].[ErrorLog] (
+    [ErrorLogID]     INT                IDENTITY (1, 1) NOT NULL,
+    [ErrorTime]      DATETIMEOFFSET (7) NOT NULL,
+    [UserName]       [sysname]          NOT NULL,
+    [ErrorNumber]    INT                NOT NULL,
+    [ErrorSeverity]  INT                NULL,
+    [ErrorState]     INT                NULL,
+    [ErrorProcedure] NVARCHAR (126)     NULL,
+    [ErrorLine]      INT                NULL,
+    [ErrorMessage]   NVARCHAR (4000)    NOT NULL,
+    [MessageBody]    XML                NULL,
+    CONSTRAINT [PK_ErrorLog_ErrorLogID] PRIMARY KEY CLUSTERED ([ErrorLogID] ASC)
+);
+GO
